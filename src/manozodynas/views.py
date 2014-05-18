32e37f52d80ed883d76bb37d4aba74285545a846
LukasTy <llukas.tyla@gmail.com>
2014-05-18 08:28:40 +0300

    Initial commit. Random word and random words views implemented.

--- src/manozodynas/views.py
+++ src/manozodynas/views.py
@@ -1,5 +1,25 @@
+"""
+Prideti forma, kurioje galima butu prideti nauja irasa i db ir poto ta irasa atvaizduotu,
+sukurti testa, sio funkcionalumo patikrinmui
+viska i github
+"""
 from django.shortcuts import render
+from manozodynas.models import Word
+from django.http import HttpResponse
 
 
 def index_view(request):
     return render(request, 'manozodynas/index.html', {})
+
+def random_word_view(request):
+	random_word = Word.get_random_word()  
+	return render(request, 'manozodynas/random.html', {"random_word":random_word.word})
+
+def random_words_view(request, count):
+	random_words = Word.get_random_words(count)
+	if (random_words):
+		return render(request, 'manozodynas/random_words.html', 
+			{"random_words":random_words, "count":count})
+	else:
+		return HttpResponse('<h1>There was a problem processing your request.</h1>' \
+			'<h2>You might have tried to get 0 random words.</h2>')
