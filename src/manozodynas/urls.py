b2bad52d5668ab9103c6c79a4d9e81b23e96b351
LukasTy <llukas.tyla@gmail.com>
2014-05-18 08:31:13 +0300

    Initial commit. Added two url paterns for random_word(/*).

--- src/manozodynas/urls.py
+++ src/manozodynas/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import index_view
+from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', index_view, name='index'),
+    url(r'^random_word/$', random_word_view, name='random_word'),
+    url(r'^random_word/(?P<count>\d+)$', random_words_view, name='random_words'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
