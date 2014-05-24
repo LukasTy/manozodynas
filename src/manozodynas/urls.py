b735f266daee5277b4654e73fac3f5a44af0b53e
LukasTy <llukas.tyla@gmail.com>
2014-05-25 02:19:07 +0300

    Added url patterns for add word form and all word pages.

--- src/manozodynas/urls.py
+++ src/manozodynas/urls.py
@@ -3,12 +3,14 @@ from django.conf import settings
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import *
+from .views import index_view, random_word_view, random_words_view, words_view, AddWord
 
 urlpatterns = patterns('',
     url(r'^$', index_view, name='index'),
     url(r'^random_word/$', random_word_view, name='random_word'),
     url(r'^random_word/(?P<count>\d+)$', random_words_view, name='random_words'),
+    url(r'^add_word/$', AddWord.as_view(), name='add_word'),
+    url(r'^words/$', words_view, name='words'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
