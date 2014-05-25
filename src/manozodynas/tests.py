e2d9af58cfbb8abafe72cd99701ae2e14d52bcd6
LukasTy <llukas.tyla@gmail.com>
2014-05-25 03:50:23 +0300

    Add WordTestCase with 3 test methods.

--- src/manozodynas/tests.py
+++ src/manozodynas/tests.py
@@ -8,3 +8,20 @@ class IndexTestCase(StatefulTesting):
     def test_index_page(self):
         self.open(reverse('index'))
         self.assertStatusCode(200)
+
+class WordTestCase(StatefulTesting):
+	def test_random_word_page(self):
+		self.open(reverse('random_word'))
+		self.assertStatusCode(200)
+
+	def test_new_word(self):
+		self.open(reverse('add_word'))
+		self.selectForm('#add_word-form')
+		self.submitForm({
+			'Speaker': 'Garsiakalbis'
+			})
+		self.assertStatusCode(200)
+
+	def test_words_page(self):
+		self.open(reverse('words'))
+		self.assertStatusCode(200)
